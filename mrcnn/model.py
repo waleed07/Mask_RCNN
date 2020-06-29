cdcd22005075afdd239ee6ba08bb14a56795e547
waleed07 <waleed.hassang10@gmail.com>
2020-06-29 17:07:47 +0500

    Add all my files

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2339,10 +2339,8 @@ class MaskRCNN():
         callbacks = [
             keras.callbacks.TensorBoard(log_dir=self.log_dir,
                                         histogram_freq=0, write_graph=True, write_images=False),
-            # keras.callbacks.ModelCheckpoint(self.checkpoint_path,
-            #                                 verbose=0, save_weights_only=True),
             keras.callbacks.ModelCheckpoint(self.checkpoint_path,
-                                            monitor='loss', verbose=1, save_best_only=True, mode='min'),
+                                            verbose=0, save_weights_only=True),
         ]
 
         # Add custom callbacks to the list
